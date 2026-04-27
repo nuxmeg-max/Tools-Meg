288dc20bd17f4cea0b5a91c48d403947c7f63220
nuxmeg-max <nuxmeg@gmail.com>
2026-04-27 15:50:53 +0700

    Remove comment about font usage in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Meg — Download TikTok, Instagram, YouTube. Remove Background. Style Text. All in one." />
-        {/* Profil-meg fonts: Barlow Condensed + IBM Plex Mono + Noto Sans */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
